aab496c01fd6d5157c1383d38ae2bfc5e1c6ff98
dakookie <damaris.suquillo@epn.edu.ec>
2023-01-08 23:22:51 -0500

    Sobrecarga

--- CursoPOO/src/ClasesYObjetos/MainPersona.java
+++ CursoPOO/src/ClasesYObjetos/MainPersona.java
@@ -3,7 +3,9 @@ package ClasesYObjetos;
 public class MainPersona {
     public static void main(String[] args) {
         Persona p1 = new Persona("Damaris", 19);
+        p1.correr();
 
-        p1.mostrarDatos();
+        Persona p2 = new Persona("1754021101");
+        p2.correr(100);
     }
 }
--- CursoPOO/src/ClasesYObjetos/Persona.java
+++ CursoPOO/src/ClasesYObjetos/Persona.java
@@ -4,6 +4,7 @@ public class Persona {
     //Atributos
     String nombre;
     int edad;
+    String dni;
 
     //Metodo constructor
     // deben ser publicos
@@ -16,5 +17,18 @@ public class Persona {
     public void mostrarDatos(){
         System.out.println("El nombre es: " + nombre);
         System.out.println("La edad es: " + edad);
+    }
+
+    //Metodo con sobrecarga siempre y cuando los parametros sean de diferente tipo
+    public Persona(String dni){
+        this.dni = dni;
+    }
+
+    public void correr(){
+        System.out.println("Soy " + nombre + " tengo " + edad + " años y estoy corriendo una maraton");
+    }
+
+    public void correr(int km){
+        System.out.println("He corrido " + km + " Kilometros");
     }
 }
