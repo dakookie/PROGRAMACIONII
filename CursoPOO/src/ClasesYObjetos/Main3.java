ed5b344e0fadcb817f9ab3b421f1cb2f9dbd97ce
dakookie <damaris.suquillo@epn.edu.ec>
2023-01-08 23:01:19 -0500

    Metodos que retornar valor

--- CursoPOO/src/ClasesYObjetos/Main3.java
+++ CursoPOO/src/ClasesYObjetos/Main3.java
@@ -12,7 +12,7 @@ public class Main3 {
     int n = sc.nextInt();
 
     Operacion3 op3 = new Operacion3();
-
+    /*
     int suma = op3.sumar(n1,n);
     int resta = op3.restar(n1, n);
     int multiplicacion = op3.multiplicar(n1, n);
@@ -20,5 +20,12 @@ public class Main3 {
 
     op3.mostrarResultados(suma, resta, multiplicacion, division);
     }
-    
+    */
+
+    //Otra forma
+    System.out.println("La suma es: " + op3.sumar(n1, n));
+    System.out.println("La resta es: " + op3.restar(n1, n));
+    System.out.println("La multiplicacion es: " + op3.multiplicar(n1, n));
+    System.out.println("La division es: " + op3.dividr(n1, n));
+    }
 }
