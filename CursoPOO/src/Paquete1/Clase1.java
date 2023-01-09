b14e9db220397ecf9a3d790b55c6379f1706c232
dakookie <damaris.suquillo@epn.edu.ec>
2023-01-08 23:30:38 -0500

    Modificadores de acceso

---
+++ CursoPOO/src/Paquete1/Clase1.java
@@ -0,0 +1,12 @@
+//Modificadores de acceso
+package Paquete1;
+
+public class Clase1 {
+    //Este atributo puede ser usado por cualquier clase del mismo paquete o diferente
+    //public int atributo1;
+
+    //Este atributo puede ser usado por cualquier clase del mismo paquete o diferente
+    private int atributo1;
+
+    
+}
