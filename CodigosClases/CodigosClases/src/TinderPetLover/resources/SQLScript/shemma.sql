db4b3389cfcdcfd8f7ec25d4b191569a6beb8611
dakookie <damaris.suquillo@epn.edu.ec>
2023-01-22 14:37:40 -0500

    Tablas creadas y cambio de base de datos

--- CodigosClases/CodigosClases/src/TinderPetLover/resources/SQLScript/shemma.sql
+++ CodigosClases/CodigosClases/src/TinderPetLover/resources/SQLScript/shemma.sql
@@ -1,3 +1,7 @@
+/*
+* @ author: dakookie: 20-ene-2023
+* Script de base de datos
+*/
 -- database: TinderPet.db
 //-- database: /path/to/database.db
 
@@ -12,16 +16,28 @@ CREATE TABLE PERSONA
     NOMBRE     VARCHAR(10),
     APELLIDO   VARCHAR(10)
 );
---DROP TABLE PERSONA;
+-- DROP TABLE PERSONA;
+-- DROP TABLE PET;
 
-INSERT INTO PERSONA (ID,   NOMBRE,   APELLIDO) 
-            VALUES  (1, "Pepe Lucho", "Perez");
-INSERT INTO PERSONA (ID,   NOMBRE,   APELLIDO) 
-            VALUES  (2, "Ana", "Suarez");
-INSERT INTO PERSONA (ID,   NOMBRE,   APELLIDO) 
-            VALUES  (3, "Juan", "Sanchez");
 
-SELECT * FROM PERSONA
+INSERT INTO PERSONA (ID,   NOMBRE,   APELLIDO)  VALUES  (1, "Pepe Lucho", "Perez");
+INSERT INTO PERSONA (ID,   NOMBRE,   APELLIDO)  VALUES  (2, "Ana", "Suarez");
+INSERT INTO PERSONA (ID,   NOMBRE,   APELLIDO)  VALUES  (3, "Juan", "Sanchez");
+
+SELECT * FROM PERSONA;
+----------------------------------------------------------
+CREATE TABLE PET
+(
+    ID         INTEGER PRIMARY KEY,
+    NOMBRE     VARCHAR(10),
+    EDAD       INTEGER
+);
+--DROP TABLE PET;
+INSERT INTO PET (ID,   NOMBRE,   EDAD)  VALUES  (1, "VALUMA", 1);
+INSERT INTO PET (ID,   NOMBRE,   EDAD)  VALUES  (2, "JUANA", 2);
+INSERT INTO PET (ID,   NOMBRE,   EDAD)  VALUES  (3, "COMOTU", 3);
+SELECT * FROM PET;
+------------------------------------------------------------------
 
 
 
